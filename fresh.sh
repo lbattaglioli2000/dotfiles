3b2898bfaa35356d33f2b798b700f6aea754ec47
Luigi Battaglioli <him@theluigi.com>
2022-04-20 01:57:44 -0400

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -45,7 +45,6 @@ $HOME/.composer/vendor/bin/global-ray install
 mkdir $HOME/Sites
 
 # Create sites subdirectories
-mkdir $HOME/Sites/blade-ui-kit
 mkdir $HOME/Sites/laravel
 
 # Clone Github repositories
